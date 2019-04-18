ecfada79eec3eec89a9ba1c939fcc7fb6a9dfd95
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-04-18 10:29:10 -0700

    updating teardown order

--- scripts/database/teardown.sql
+++ scripts/database/teardown.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS info_depot_item;
-DROP TABLE IF EXISTS info_depot_course;
 DROP TABLE IF EXISTS info_depot_item_artifact;
 DROP TABLE IF EXISTS info_depot_comment;
 DROP TABLE IF EXISTS info_depot_found_helpful;
 DROP TABLE IF EXISTS info_depot_found_unhelpful;
+DROP TABLE IF EXISTS info_depot_item;
+DROP TABLE IF EXISTS info_depot_course;
