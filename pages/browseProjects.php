ed44720b4efbec5655dc5a0980575d8a3c17effe
ramossy <ramossy@oregonstate.edu>
2019-04-22 15:22:24 -0700

    Fix configurations to enable DB access in pages folder.

--- pages/browseProjects.php
+++ pages/browseProjects.php
@@ -1,2 +1,11 @@
 hello
 good-bye
+
+<?php
+$dao = new DataAccess\UsersDao($dbConn);
+$types = $dao->getUserTypes();
+foreach ($types as $type) {
+    echo $type->getId() . ' - ' . $type->getName() . ' ';
+}
+
+?>
