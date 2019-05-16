2a87f51b500511dd9e52d07a52edbe07df06b511
ramossy <ramossy@oregonstate.edu>
2019-05-16 13:42:20 -0700

    Squashed 'lib/shared/' changes from 6f21502..4a322a2
    4a322a2 Move Keywords to shared folder.
    491a02e adding argument documentation
    77bbdc8 checking for set variables, removing cleaning in model
    25a246a adding Github enum
    64ad3ac creating more general oauth  authenticator
    1c172d0 updating shared libraries
    c9351c6 removing ability to handle redirect, adding documentation
    2bb0963 adding back query string check
    d178635 refactoring ONID auth into function
    144950f removing need for 'provider' in query string
    d683948 URL encode only redirect
    035422b encoding URL in service callback
    8f47514 restricting query string parameters
    141c3b6 adding the query string to the url
    566f9f4 allowing newlines to be escaped in headers
    37dd222 adding missing CRLF to headers end
    fc38339 fixing HTML content type
    b58d73c adding backwards compatability for PHP 5.3
    28c00ef adding check for constant added after PHP 5.4
    81b18fc removing unneccessary checks from model
    a73f0f1 Merge commit 'df2d68e6f0fb913649c175f091436e9bee546e1e'
    0aa5ccf fixing bug with HTML encoding adding newlines
    de45f9d implementing output security for user inputs
    254b213 escaping HTML to prevent XSS attacks
    2fc7e55 Merge branch 'stage' of https://github.com/osu-tekbots/capstone-website into stage
    d40f00f Implement ONID Input funtionality for new users.
    f708059 adding debugging, better HTML support
    150e47a adding HTML email capabilities
    1eab68f implementing generic ONID login provider
    685ad48 adding full name helper function
    16221e8 adding helper get() function
    fa9faad updating action handler logger visibility
    15ddb5f moving authentication into shared library
    7166c1f fixing bug with missing return value from extract
    a703f7a making response code compatable with PHP 5.3
    86e82fc changing enumeration name
    e9e3658 fixing login error
    73b1587 setting default enumeration types on construction
    244a9fa using backwards compatable object creation
    ecc53d9 adding auth provider constants
    219ca70 fixing error in getAllUsers
    ce03bb6 adding enum constants for comparision
    2843b6b adding config for emails
    c9daaff implementing generic email provider class
    44af2c2 fixing syntax error
    a871647 changing constant declaration, but in serialize
    57c96dc changing logger visibility, adding utility functions
    
    git-subtree-dir: lib/shared
    git-subtree-split: 4a322a24209eab90efb4aa0ef7937fb057b4c2c8

--- classes/Model/Keyword.php
+++ classes/Model/Keyword.php
@@ -1,23 +1,23 @@
 <?php
 namespace Model;
 
-/**
- * Data class representing a user salutation enumeration
- */
-class UserSalutation {
-    const NONE = 1;
+class Keyword {
     
     /** @var integer */
     private $id;
     
     /** @var  string */
     private $name;
+	
+	private $parentId;
+	
+	private $approved;
 
     /**
-     * Constructs a new instance of a UserSalutation.
+     * Constructs a new instance of a CapstoneProjectFocus.
      *
-     * @param integer $id the ID of the salutation. This should come directly from the database.
-     * @param string $name the name associated with the salutation
+     * @param integer $id the ID of the CapstoneProjectFocus. This should come directly from the database.
+     * @param string $name the name associated with the CapstoneProjectFocus
      */
     public function __construct($id = null, $name = null) {
         if ($id == null && $name == null) {
@@ -62,6 +62,42 @@ class UserSalutation {
      */ 
     public function setName($name) {
         $this->name = $name;
+
+        return $this;
+    }
+	
+	    /**
+     * Get the value of parent id
+     */ 
+    public function getParentId() {
+        return $this->parentId;
+    }
+
+    /**
+     * Set the value of parent id
+     *
+     * @return  self
+     */ 
+    public function setParentId($id) {
+        $this->parentId = $id;
+
+        return $this;
+    }
+	
+	 /**
+     * Get the value of id
+     */ 
+    public function getApproved() {
+        return $this->approved;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setApproved($approved) {
+        $this->approved = $approved;
 
         return $this;
     }
